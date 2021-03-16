e393cc92ed93f81c3c4858e7dde583c4e4c07ae4
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-15 20:30:33 -0500

    variables- operaciones matematicas basicas

--- semana4/dia1/variables/codigo.js
+++ semana4/dia1/variables/codigo.js
@@ -7,20 +7,55 @@ multilineas en Javascript
 
 //variable de tipo texto
 let nombre = "Josue";
-let sistemaOperativo = "linux";
-console.log(typeof nombre); // esto me devuelve string
 
-// alert(
-//   "Mi segundo nombre es" +
-//     nombre +
-//     "y mi sistema operativo favorito es" +
-//     sistemaOperativo
-// );
+//variables de tipo numero
+let miEdad = 24;
+let miPeso = 75.4;
 
-console.log("2"*3)
+// 1.creo una variable para mi altura
+// 2. creo una variable para mi peso
+// 3. creo una variable que divida mi masa corporal entre mi altura al cuadrado
 
-console.log("1"+3)
+let altura = 1.7;
+let peso = 75.4;
 
-alert(
-  `Mi segundo nombre es ${nombre} y mi sistema favorito es ${sistemaOperativo}`
-);
+//bmi= indice de masa corporal
+let bmi = peso / altura ** 2;
+
+alert(bmi);
+
+// hay 5 operaciones basicas
+
+// la suma +
+// la multiplicacion *
+// la division /
+// la resta -
+// el modulo
+
+let a = 6;
+let b = 10;
+
+let suma = a + b;
+console.log(suma);
+
+let resta = b - a;
+console.log(resta);
+
+let division = b / a;
+console.log(division);
+
+let multiplicacion = a * b;
+console.log(multiplicacion);
+
+let potenciacion = a ** b;
+console.log(potenciacion);
+
+// 0.5 = 1/2
+let radicacion = a ** 0.5;
+console.log(radicacion);
+
+//el modulo es el resto de una division
+let modulo = a % b;
+
+
+console.log(modulo);
