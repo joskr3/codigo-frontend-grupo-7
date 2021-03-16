237ac991b7259a5f892109a1349461d1ea1deeac
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-15 20:02:14 -0500

    mi primer codigo en javascript

---
+++ semana4/dia1/variables/codigo.js
@@ -0,0 +1,26 @@
+// este es un comentario de una linea
+
+/* 
+Este es un comentario
+multilineas en Javascript
+*/
+
+//variable de tipo texto
+let nombre = "Josue";
+let sistemaOperativo = "linux";
+console.log(typeof nombre); // esto me devuelve string
+
+// alert(
+//   "Mi segundo nombre es" +
+//     nombre +
+//     "y mi sistema operativo favorito es" +
+//     sistemaOperativo
+// );
+
+console.log("2"*3)
+
+console.log("1"+3)
+
+alert(
+  `Mi segundo nombre es ${nombre} y mi sistema favorito es ${sistemaOperativo}`
+);
