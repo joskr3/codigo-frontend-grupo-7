63bf6236f537eea40dcde9aa91f153afbd214882
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-17 22:06:54 -0500

    usando metodos de los arrays

--- semana4/dia3/arreglos/codigo.js
+++ semana4/dia3/arreglos/codigo.js
@@ -41,7 +41,96 @@ mi_arreglo[5] // me da undefined
 
 mi_arreglo.lenght // la longitud no cantidad de elementos de mi arreglo
 
-mi_arreglo[mi_arreglo.lenght - 1 ]
+mi_arreglo[mi_arreglo.length - 1 ]
+
+
+console.log("mi ultimo elemento es :",mi_arreglo[mi_arreglo.length - 1 ])
+
+
+
+//agregar un elemento al final de mi array
+
+mi_arreglo.push("elemento4")
+console.log(mi_arreglo)
+
+console.log("mi ultimo elemento es :",mi_arreglo[mi_arreglo.length - 1 ])
+
+
+
+//voy a agregar un elemento al final de mi array 
+//push(elemento que voy a agregar)
+
+mi_arreglo.push("elemento5")
+
+console.log(mi_arreglo[mi_arreglo.length-1],": Este es mi nuevo elemento")
+
+
+
+
+
+//voy a eliminar el ultimo elemento de mi array
+//hago uso del metodo pop()
+
+mi_arreglo.pop()
+console.log(mi_arreglo[mi_arreglo.length-1],": Este es mi ultimo nuevo elemento")
+
+
+
+
+
+
+
+//vamos a agregar un elemento al inicio del array
+//usamos unshift("elemento a agregar al inicio")
+
+
+mi_arreglo.unshift("elemento0")
+
+console.log("Mi nuevo primer elemento es : ", mi_arreglo[0])
+
+
+
+
+
+
+
+
+
+
+// vamos a eliminar el primer elemento de mi array
+//shift()
+
+console.log(mi_arreglo[0], "Mi primer elemento antes de elimnarlo con shift()")
+mi_arreglo.shift()
+
+console.log(mi_arreglo[0], "es mi primer elemento ahora")
+
+
+
+
+
+
+
+
+
+
+//necesito averiguar el indice de cierto elemento
+//con indexOf("elemento")
+
+console.log("El indice de mi elemento3 es: ",mi_arreglo.indexOf("elemento3"))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
