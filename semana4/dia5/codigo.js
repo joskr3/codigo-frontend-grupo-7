ba173b11b15092a3b1ec1a5028ca03bb096c1c11
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-19 20:45:00 -0500

    funciones anidad

--- semana4/dia5/codigo.js
+++ semana4/dia5/codigo.js
@@ -6,18 +6,25 @@ function sumar(numero1, numero2) {
   return poner_Html;
 }
 
-function sumar1(numero1, numero2) {
+const sumar_dos_valores = function sumar1(numero1, numero2) {
   let total = numero1 + numero2;
+  return total;
+};
 
-  console.log(total);
-
-  //return total + 1000;
-}
+console.log("Mi suma es:", sumar_dos_valores(4, 4));
 
 function restar(numero1, numero2) {
   console.log(numero1 - numero2);
   return numero1 - numero2;
 }
+
+const multiplicado = function multiplicar(numero1, numero2) {
+  return numero1 * numero2;
+};
+
+let y = multiplicado(1, 3);
+
+console.log("Mi multiplicacion es : ", y);
 
 function multiplicar(numero1, numero2) {
   return numero1 * numero2;
@@ -43,3 +50,80 @@ function ecu_cuadratica(a, b, c) {
 }
 
 ecu_cuadratica(56, 34, 5);
+
+//recursividad
+
+//bucle - while
+
+let contador = 0;
+
+while (contador < 10) {
+  console.log(contador);
+  contador++;
+}
+
+//funcion recursiva
+
+function recursividad(contador) {
+  if (contador >= 10) {
+    return;
+  }
+  //console.log(contador);
+  recursividad(contador + 1);
+}
+
+//recursividad(9)
+
+//funciones anidadas
+
+function sumarCuadrados(a, b) {
+  function cuadrado(x) {
+    return x * x;
+  }
+  return cuadrado(a) + cuadrado(b);
+}
+
+console.log(sumarCuadrados(2, 3));
+
+//(a + b)4 = a4 + 4a3b + 6a2b2 + 4ab3 + b4
+
+const binomio_de_newton_4 = function binomio_cuarta(a, b) {
+  function suma_cuarta(a, b) {
+    return a ** 4 + b ** 4;
+  }
+  function suma_cubo(a, b) {
+    return 4 * (a ** 3 * b) + 4 * (a * b ** 3);
+  }
+  function multi_cuadrados(a, b) {
+    return 6 * (a ** 2 * b ** 2);
+  }
+
+  return suma_cuarta(a, b) + suma_cubo(a, b) + multi_cuadrados(a, b);
+};
+
+
+const binomio_de_newton_4 = function binomio_cuarta(a, b) {
+  function suma_cuarta(a, b) {
+    return a ** 4 + b ** 4;
+  }
+  function suma_cubo(a, b) {
+    return 4 * (a ** 3 * b) + 4 * (a * b ** 3);
+  }
+  function multi_cuadrados(a, b) {
+    return 6 * (a ** 2 * b ** 2);
+  }
+
+  return suma_cuarta(a, b) + suma_cubo(a, b) + multi_cuadrados(a, b);
+};
+
+const binomio_de_newton_4___2 = function binomio_cuarta(a, b) {
+  return (
+    a ** 4 +
+    b ** 4 +
+    4 * (a ** 3 * b) +
+    4 * (a * b ** 3) +
+    6 * (a ** 2 * b ** 2)
+  );
+};
+
+console.log(binomio_de_newton_4(2, 3));
