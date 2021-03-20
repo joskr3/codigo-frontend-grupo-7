d758a299dde5ba304bf48347791cae9d8b317d53
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-19 19:26:40 -0500

    trabajando con funciones

---
+++ semana4/dia5/codigo.js
@@ -0,0 +1,38 @@
+//Funciones
+// ``
+function sumar(numero1, numero2) {
+  let total = numero1 + numero2;
+  let poner_Html = document.write(`<h1>${total}</h1>`);
+  return poner_Html;
+}
+
+function sumar1(numero1, numero2) {
+  let total = numero1 + numero2;
+
+  console.log(total);
+
+  //return total + 1000;
+}
+
+function restar(numero1, numero2) {
+  console.log(numero1 - numero2);
+  return numero1 - numero2;
+}
+
+function multiplicar(numero1, numero2) {
+  return numero1 * numero2;
+}
+
+function ecu_cuadratica(a, b, c) {
+  let discriminante = b ** 2 - 4 * a * c;
+  let x = 0;
+  if (discriminante > 0) {
+    x = -b + (Math.sqrt(discriminante) / 2) * a;
+    let escribir_formula = document.write(`<h1>${x}</h1>`);
+    return escribir_formula;
+  } else {
+    return "Error, ingresa valores validos";
+  }
+}
+
+ecu_cuadratica(56, 34, 5);
