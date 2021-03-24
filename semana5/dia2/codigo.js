bfa2be56660c4fd26da0a73dbae3b5fdacc4aca1
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-23 21:44:14 -0500

    ref

--- semana5/dia2/codigo.js
+++ semana5/dia2/codigo.js
@@ -91,46 +91,6 @@ let cadenaSinMultiLinea =
 
 console.log("1", cadenaMultilinea, "2", cadenaSinMultiLinea);
 
-// vamos a hacer  una funcion que reciba como parametro una letra de una cancion y cuente cada una de las vocales por separado
-
-//Recibo como parametro un string(cadena de texto)
-function contarVocales(cancion) {
-  //creo un objeto dentro de mi funcion donde voy a  alamacenar la cantidad de veces que una letra se repite en la cancion(string)
-  let contador = {
-    a: 0,
-    e: 0,
-    i: 0,
-    o: 0,
-    u: 0,
-    otros: 0,
-  };
-
-  // un iteradot que contine una variable que inicializara en cero y que tiene una condiuion que dice : mienstras esta variable creaada dentro
-  // del for sea menor a la longitud de mi cancion(array/string)  y voy aumentar estar variable en 1 cada vez , hasta que esa
-  // condicion deje de ser verdadera
-
-  for (let i = 0; i < cancion.length; i++) {
-    //voy a evaluar cada caracter en mi cancion y compararlo con lo que quiero buscar que en este caso es "a"(convertida a minuscula por lowercase)
-    if (cancion[i].toLowerCase() === "a") {
-      // voy a amuentar en 1 el contador para a si esta condicion se cumnple
-      contador.a += 1;
-    } else if (cancion[i].toLowerCase() === "e") {
-      contador.e += 1;
-    } else if (cancion[i].toLowerCase() === "i") {
-      contador.i += 1;
-    } else if (cancion[i].toLowerCase() === "o") {
-      contador.o += 1;
-    } else if (cancion[i].toLowerCase() === "u") {
-      contador.u += 1;
-    } else {
-      contador.otros += 1;
-    }
-  }
-  // y termino min funcion dandole un valor que en este caso es todo mi objeto contador , 
-  //que a su vez contiene los contadores de cada letra 
-  return contador;
-}
-
 const miCancion = `How many roads must a man walk down
 Before you call him a man?
 How many seas must a white dove sail
@@ -156,7 +116,57 @@ That too many people have died?
 The answer, my friend, is blowin' in the wind
 The answer is blowin' in the wind`;
 
-function contarVocales2(cancion) {
+// vamos a hacer  una funcion que reciba como parametro una letra de una cancion y cuente cada una de las vocales por separado
+
+//Recibo como parametro un string(cadena de texto)
+function contarVocales(cancion) {
+  //creo un objeto dentro de mi funcion donde voy a  alamacenar la cantidad de veces que una letra se repite en la cancion(string)
+  let contador = {
+    a: 0,
+    e: 0,
+    i: 0,
+    o: 0,
+    u: 0,
+    otros: 0,
+  };
+
+  // un iterador que contine una variable que inicializara en cero y que tiene una condiuion que dice : mienstras esta variable creaada dentro
+  // del for sea menor a la longitud de mi cancion(array/string)  y voy aumentar estar variable en 1 cada vez , hasta que esa
+  // condicion deje de ser verdadera
+
+  for (let i = 0; i < cancion.length; i++) {
+    //voy a evaluar cada caracter en mi cancion y compararlo con lo que quiero buscar que en este caso es "a"(convertida a minuscula por lowercase)
+    if (cancion[i].toLowerCase() === "a") {
+      // voy a amuentar en 1 el contador para a si esta condicion se cumnple
+      contador.a += 1;
+    } else if (cancion[i].toLowerCase() === "e") {
+      contador.e += 1;
+    } else if (cancion[i].toLowerCase() === "i") {
+      contador.i += 1;
+    } else if (cancion[i].toLowerCase() === "o") {
+      contador.o += 1;
+    } else if (cancion[i].toLowerCase() === "u") {
+      contador.u += 1;
+    } else {
+      contador.otros += 1;
+    }
+  }
+  // y termino min funcion dandole un valor que en este caso es todo mi objeto contador , 
+  //que a su vez contiene los contadores de cada letra 
+  return contador;
+}
+
+// #################################################################################
+// #################################################################################
+// #################################################################################
+// #################################################################################
+// #################################################################################
+// #################################################################################
+// #################################################################################
+// #################################################################################
+// #################################################################################
+
+function contarVocalesConSwitchCase(cancion) {
   let contador = {
     a: 0,
     e: 0,
@@ -192,7 +202,7 @@ function contarVocales2(cancion) {
   return contador;
 }
 
-const cuentaLetras = contarVocales2(miCancion);
+const cuentaLetras = contarVocalesConSwitchCase(miCancion);
 
 console.log(`
   a: ${cuentaLetras.a},
