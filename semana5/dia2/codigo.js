ac130b402766f6000ec6e6beceb5948c432ddf3e
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-23 21:23:45 -0500

    trabajando con strings

--- semana5/dia2/codigo.js
+++ semana5/dia2/codigo.js
@@ -91,20 +91,28 @@ let cadenaSinMultiLinea =
 
 console.log("1", cadenaMultilinea, "2", cadenaSinMultiLinea);
 
-// vamoas a hacer  una funcion que reciba como parametro una letra de una cancion y cuente cada una de las vocales por separado
+// vamos a hacer  una funcion que reciba como parametro una letra de una cancion y cuente cada una de las vocales por separado
 
+//Recibo como parametro un string(cadena de texto)
 function contarVocales(cancion) {
+  //creo un objeto dentro de mi funcion donde voy a  alamacenar la cantidad de veces que una letra se repite en la cancion(string)
   let contador = {
-    a:0,
-    e:0,
-    i:0,
-    o:0,
-    u:0,
-    otros:0,
+    a: 0,
+    e: 0,
+    i: 0,
+    o: 0,
+    u: 0,
+    otros: 0,
   };
 
+  // un iteradot que contine una variable que inicializara en cero y que tiene una condiuion que dice : mienstras esta variable creaada dentro
+  // del for sea menor a la longitud de mi cancion(array/string)  y voy aumentar estar variable en 1 cada vez , hasta que esa
+  // condicion deje de ser verdadera
+
   for (let i = 0; i < cancion.length; i++) {
+    //voy a evaluar cada caracter en mi cancion y compararlo con lo que quiero buscar que en este caso es "a"(convertida a minuscula por lowercase)
     if (cancion[i].toLowerCase() === "a") {
+      // voy a amuentar en 1 el contador para a si esta condicion se cumnple
       contador.a += 1;
     } else if (cancion[i].toLowerCase() === "e") {
       contador.e += 1;
@@ -118,6 +126,8 @@ function contarVocales(cancion) {
       contador.otros += 1;
     }
   }
+  // y termino min funcion dandole un valor que en este caso es todo mi objeto contador , 
+  //que a su vez contiene los contadores de cada letra 
   return contador;
 }
 
@@ -146,7 +156,43 @@ That too many people have died?
 The answer, my friend, is blowin' in the wind
 The answer is blowin' in the wind`;
 
-const cuentaLetras = contarVocales(miCancion);
+function contarVocales2(cancion) {
+  let contador = {
+    a: 0,
+    e: 0,
+    i: 0,
+    o: 0,
+    u: 0,
+    otros: 0,
+  };
+  let i = 0;
+  while (i < cancion.length) {
+    switch (cancion[i].toLowerCase()) {
+      case "a":
+        contador.a += 1;
+        break;
+      case "e":
+        contador.e += 1;
+        break;
+      case "i":
+        contador.i += 1;
+        break;
+      case "o":
+        contador.o += 1;
+        break;
+      case "u":
+        contador.u += 1;
+        break;
+      default:
+        contador.otros += 1;
+        break;
+    }
+    i++;
+  }
+  return contador;
+}
+
+const cuentaLetras = contarVocales2(miCancion);
 
 console.log(`
   a: ${cuentaLetras.a},
