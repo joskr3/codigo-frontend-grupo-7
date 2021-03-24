631aaf4f0fd41fba78b8cbbb781ef3dcff5fa945
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-03-23 20:39:38 -0500

    trabajando con strings

--- semana5/dia2/codigo.js
+++ semana5/dia2/codigo.js
@@ -91,6 +91,76 @@ let cadenaSinMultiLinea =
 
 console.log("1", cadenaMultilinea, "2", cadenaSinMultiLinea);
 
-
-
 // vamoas a hacer  una funcion que reciba como parametro una letra de una cancion y cuente cada una de las vocales por separado
+
+function contarVocales(cancion) {
+  let contador = {
+    a:0,
+    e:0,
+    i:0,
+    o:0,
+    u:0,
+    otros:0,
+  };
+
+  for (let i = 0; i < cancion.length; i++) {
+    if (cancion[i].toLowerCase() === "a") {
+      contador.a += 1;
+    } else if (cancion[i].toLowerCase() === "e") {
+      contador.e += 1;
+    } else if (cancion[i].toLowerCase() === "i") {
+      contador.i += 1;
+    } else if (cancion[i].toLowerCase() === "o") {
+      contador.o += 1;
+    } else if (cancion[i].toLowerCase() === "u") {
+      contador.u += 1;
+    } else {
+      contador.otros += 1;
+    }
+  }
+  return contador;
+}
+
+const miCancion = `How many roads must a man walk down
+Before you call him a man?
+How many seas must a white dove sail
+Before she sleeps in the sand?
+Yes, and how many times must the cannonballs fly
+Before they're forever banned?
+The answer, my friend, is blowin' in the wind
+The answer is blowin' in the wind
+Yes, and how many years must a mountain exist
+Before it is washed to the sea?
+And how many years can some people exist
+Before they're allowed to be free?
+Yes, and how many times can a man turn his head
+And pretend that he just doesn't see?
+The answer, my friend, is blowin' in the wind
+The answer is blowin' in the wind
+Yes, and how many times must a man look up
+Before he can see the sky?
+And how many ears must one man have
+Before he can hear people cry?
+Yes, and how many deaths will it take 'til he knows
+That too many people have died?
+The answer, my friend, is blowin' in the wind
+The answer is blowin' in the wind`;
+
+const cuentaLetras = contarVocales(miCancion);
+
+console.log(`
+  a: ${cuentaLetras.a},
+  e: ${cuentaLetras.e},
+  i: ${cuentaLetras.i},
+  o: ${cuentaLetras.o},
+  u: ${cuentaLetras.u},
+  otros: ${cuentaLetras.otros},
+  total: ${
+    cuentaLetras.a +
+    cuentaLetras.e +
+    cuentaLetras.i +
+    cuentaLetras.o +
+    cuentaLetras.u +
+    cuentaLetras.otros
+  },
+`);
