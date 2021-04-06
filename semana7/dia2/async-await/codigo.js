dcbc331e07c83d70d2b4c4482bfed99de0f4cbbe
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-06 18:44:14 -0500

    trabajando con async/await

--- semana7/dia2/async-await/codigo.js
+++ semana7/dia2/async-await/codigo.js
@@ -6,8 +6,41 @@ async function fun_asincrona() {
 //funcion flecha  asincrona
 const otraFuncion = async () => 42;
 
-console.log(fun_asincrona());
+//console.log(fun_asincrona());
 
 fun_asincrona().then((valor) => {
   console.log("Este es el valor de la promesa", valor);
+});
+
+const valor = otraFuncion();
+console.log(valor);
+
+async function obtenerValor() {
+  const value = await otraFuncion();
+  return value;
+}
+
+console.log(obtenerValor);
+
+let sumar = (x, y) => {
+  return new Promise((resolve, reject) => {
+    let suma = x + y;
+    if (suma) {
+      resolve(suma);
+    } else {
+      reject(Error("Hey!!, no es una operacion valida"));
+    }
+  });
+};
+
+const sumaNum = async (a, b) => {
+  try {
+    return await a + b;
+  } catch {
+    return Error("Hey!!, no es una operacion valida");
+  }
+};
+
+sumaNum(2, "r").then((valor) => {
+  console.log(valor);
 });
