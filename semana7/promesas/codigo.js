6f11ad21e3fbf1edf7d766974679c58c899dbbc2
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-05 21:04:30 -0500

    trabajando con promesas

--- semana7/promesas/codigo.js
+++ semana7/promesas/codigo.js
@@ -78,3 +78,42 @@ sumar(2, 2)
   .catch((error) => {
     return error;
   });
+
+//binomio cuadrado
+
+const binomio = (a, b) => {
+  function parte1() {
+    return new Promise((resolve, reject) => {
+      let primerFactor = a ** 2;
+      primerFactor
+        ? resolve(primerFactor)
+        : reject(Error("No es una operacion valida-parte1"));
+    });
+  }
+  function parte2() {
+    return new Promise((resolve, reject) => {
+      let segundoFactor = 2 * a * b;
+      segundoFactor
+        ? resolve(segundoFactor)
+        : reject(Error("No es una operacion valida-parte2"));
+    });
+  }
+  function parte3() {
+    return new Promise((resolve, reject) => {
+      let tercerFactor = b ** 2;
+      tercerFactor
+        ? resolve(tercerFactor)
+        : reject(Error("No es una operacion valida-parte3"));
+    });
+  }
+
+  return parte1().then((resultadoParte1) => {
+    parte2().then((resultadoParte2) => {
+      parte3().then((resultadoParte3) => {
+        console.log(resultadoParte1 + resultadoParte2 + resultadoParte3);
+      });
+    });
+  });
+};
+
+binomio(2, "w");
