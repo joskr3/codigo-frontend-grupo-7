071b757702578fc6ace44c520153c53c8d4ae63d
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-05 20:44:39 -0500

    trabajando con promesas

--- semana7/promesas/codigo.js
+++ semana7/promesas/codigo.js
@@ -14,20 +14,7 @@
 //2.fue resuelta(se cumplio)
 //3. fue rechazada(no se cumplio)
 
-let sumar = function (x, y) {
-  return new Promise((resolve, reject) => {
-    let suma = x + y;
-    if (suma) {
-      resolve(suma);
-    } else {
-      reject(Error("Hey!!, no es una suma valida"));
-    }
-  });
-};
-
-console.log(sumar(2, 4));
-
-// let sumar = (x, y) => {
+// let sumar = function (x, y) {
 //   return new Promise((resolve, reject) => {
 //     let suma = x + y;
 //     if (suma) {
@@ -37,3 +24,57 @@ console.log(sumar(2, 4));
 //     }
 //   });
 // };
+
+let sumar = (x, y) => {
+  return new Promise((resolve, reject) => {
+    let suma = x + y;
+    if (suma) {
+      resolve(suma);
+    } else {
+      reject(Error("Hey!!, no es una operacion valida"));
+    }
+  });
+};
+
+let restar = (x, y) => {
+  return new Promise((resolve, reject) => {
+    let resta = x - y;
+    if (resta) {
+      resolve(resta);
+    } else {
+      reject(Error("Hey!!, no es una operacion valida"));
+    }
+  });
+};
+let multiplicar = (x, y) => {
+  return new Promise((resolve, reject) => {
+    let multiplicacion = x * y;
+    if (multiplicacion) {
+      resolve(multiplicacion);
+    } else {
+      reject(Error("Hey!!, no es una operacion valida"));
+    }
+  });
+};
+
+let dividir = (x, y) => {
+  return new Promise((resolve, reject) => {
+    let multiplicacion = x / y;
+    if (multiplicacion) {
+      resolve(multiplicacion);
+    } else {
+      reject(Error("Hey!!, no es una operacion valida"));
+    }
+  });
+};
+
+sumar(2, 2)
+  .then((resultadoSuma) => {
+    return restar(resultadoSuma, 4);
+  })
+  .then((resultadoResta) => {
+    return sumar(resultadoResta, 7);
+  })
+  .catch((error) => {
+    return error;
+  });
