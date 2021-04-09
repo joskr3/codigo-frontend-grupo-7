6a3d188ed6e034a43f6ffba22c48a6b3c80ccdf1
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-08 20:13:22 -0500

    trabajando fetch con axios

--- semana7/dia4/fetch/codigo.js
+++ semana7/dia4/fetch/codigo.js
@@ -13,14 +13,8 @@ fetch(url)
                 <a href="#" class="btn btn-primary">Ir al mapa</a>
          </div>
     `;
-    console.log(data);
   })
   .catch((error) => new Error(error.message));
-
-//   miUsuario.textContent = `foreach---->>>>>Hola me llamo ${usuario.nombre}, tengo ${usuario.edad} y mi color favorito es el ${usuario.colorFav}`;
-//   document.querySelector("#usuarios").appendChild(miUsuario);
-
-//async - await
 
 const generadorPokemones = async (idPokemon) => {
   try {
@@ -46,3 +40,30 @@ const generadorPokemones = async (idPokemon) => {
 };
 
 generadorPokemones(157)
+
+
+//con axios
+
+
+const generadorPokemonesAxios = async (idPokemon) => {
+    try {
+      const respuesta = await axios(
+        `https://pokeapi.co/api/v2/pokemon/${idPokemon}/`
+      );
+      const elemento = document.getElementById("elemento2");
+  
+      elemento.innerHTML = `
+      <div class="card" style="width: 18rem;">
+          <img src="${respuesta.data.sprites.front_default}" class="card-img-top" alt="mi pokemon favorito">
+           <div class="card-body">
+              <h5 class="card-title">${respuesta.data.name}</h5>
+              <p class="card-text">Mi pokemon favorito</p>
+              <a href="#" class="btn btn-primary">Ir al mapa</a>
+       </div>
+  `;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  generadorPokemonesAxios(265)
