f094b47cccfb442e185f5b3391ec53a985abda3f
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-10 11:56:09 -0500

    trabajando con map en react

--- semana7/dia5-dia6/mi-primera-app-en-codigo/src/componentes/Content/Content.jsx
+++ semana7/dia5-dia6/mi-primera-app-en-codigo/src/componentes/Content/Content.jsx
@@ -2,9 +2,27 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const Content = () => {
-  
   const [likes, aumentarLikes] = useState(0);
-
+  const [entradas, setEntradas] = useState([
+    {
+      titulo: "lomo saltado",
+      body: "Unos de los platos emblematicos de Peru",
+      chef: "Rolando",
+      id: "1",
+    },
+    {
+      titulo: "pollito a la brasa",
+      body: "Otro de los platos emblematicos de Peru",
+      chef: "Andrea",
+      id: "2",
+    },
+    {
+      titulo: "ceviche",
+      body: "Otro de los platos emblematicos de Peru",
+      chef: "Miguel",
+      id: "3",
+    },
+  ]);
 
   const manejarAumentoLikes = () => {
     aumentarLikes(likes + 1);
@@ -15,19 +33,30 @@ const Content = () => {
   // };
 
   return (
-    <div>
-      <h3>Bienvenid@s a mi publicacion</h3>
+    // <div>
+    //   <h3>Bienvenid@s a mi publicacion</h3>
 
-      <p style={{ width: "50%" }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis iste
-        omnis optio voluptas, minima deleniti excepturi vero facere veritatis
-        tenetur neque quos, perspiciatis at numquam, est consectetur
-        necessitatibus quae voluptates!
-      </p>
+    //   <p style={{ width: "50%" }}>
+    //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis iste
+    //     omnis optio voluptas, minima deleniti excepturi vero facere veritatis
+    //     tenetur neque quos, perspiciatis at numquam, est consectetur
+    //     necessitatibus quae voluptates!
+    //   </p>
 
-      <button onClick={() => manejarAumentoLikes()}>Me gusta este post</button>
+    //   <button onClick={() => manejarAumentoLikes()}>Me gusta este post</button>
 
-      <p>Likes: {likes} </p>
+    //   <p>Likes: {likes} </p>
+    // </div>
+
+    <div className="content">
+      {entradas.map((entrada) => (
+        <div className="content-preview" key={entrada.id}>
+          <h3>Bienvenid@s a mi publicacion</h3>
+          <h2>{entrada.titulo}</h2>
+          <p>Escrito por la/el cheff : {entrada.chef}</p>
+          <button onClick={() => manejarAumentoLikes()}>Me gusta</button>
+        </div>
+      ))}
     </div>
   );
 };
