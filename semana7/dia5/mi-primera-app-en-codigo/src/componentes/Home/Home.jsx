007cad3c48108ca48c7d74c5d6b8962e8b8e16e3
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-09 21:04:05 -0500

    trabajando con estiilos en react

--- semana7/dia5/mi-primera-app-en-codigo/src/componentes/Home/Home.jsx
+++ semana7/dia5/mi-primera-app-en-codigo/src/componentes/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "./styles.css";
+import lisa from "../../assets/images/lisa.jpg";
 
 const Home = () => {
   const autor = "Josue";
 
   return (
     <div>
-      <p style={{color:"red"}} >
-        Hola amigos de codiGo ,soy el creador de esta entrada {autor} 
+      <p style={{ color: "red" }}>
+        Hola amigos de codiGo ,soy el creador de esta entrada {autor}
       </p>
+      <img src={lisa} alt="imagen" />
     </div>
   );
 };
