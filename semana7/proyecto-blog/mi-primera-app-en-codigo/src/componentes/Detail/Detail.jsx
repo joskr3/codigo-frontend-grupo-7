a6415250ab4f040db6db60b23c123eabba0fb077
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-22 22:00:28 -0500

    trabajando con spectrum flex

--- semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/Detail/Detail.jsx
+++ semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/Detail/Detail.jsx
@@ -1,10 +1,12 @@
+import { Button } from "@react-spectrum/button";
+import { Flex } from "@react-spectrum/layout";
+import { Heading, Text } from "@react-spectrum/text";
 import React from "react";
 import { useParams, useHistory } from "react-router";
 import useFetch from "../../hooks/useFetch/useFetch";
 import { url } from "../../utils/utils";
 
 function Detail() {
-
   const { id } = useParams();
   const { data: blog, isLoading, error } = useFetch(`${url}${id}`);
   const history = useHistory();
@@ -23,17 +25,21 @@ function Detail() {
 
   return (
     <div>
-      {isLoading && <div>Cargando...</div>}
-      {error && <div>{error}</div>}
+      {isLoading && <Text>Cargando...</Text>}
+      {error && <Text>{error}</Text>}
       {blog && (
-        <article>
-          <h2>{blog.titulo}</h2>
-          <p>Escrito por: {blog.autor}</p>
-          <div>{blog.body}</div>
-          <button onClick={() => eliminarEntrada(blog.id)}>
+        <Flex direction="column" gap="size-100" alignItems="center">
+          <Heading level={2}>{blog.titulo}</Heading>
+          <Text>Escrito por: {blog.autor}</Text>
+          <Text>{blog.body}</Text>
+          <Button
+            maxWidth={250}
+            variant="secondary"
+            onPress={() => eliminarEntrada(blog.id)}
+          >
             ELiminar esta entrada
-          </button>
-        </article>
+          </Button>
+        </Flex>
       )}
     </div>
   );
