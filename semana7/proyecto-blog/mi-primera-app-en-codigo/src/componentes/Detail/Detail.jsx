7560bc01ff5d8fb0efde7caf8092c6f143f19c1b
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-23 19:56:00 -0500

    trabajando con sass

--- semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/Detail/Detail.jsx
+++ semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/Detail/Detail.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useParams, useHistory } from "react-router";
 import useFetch from "../../hooks/useFetch/useFetch";
 import { url } from "../../utils/utils";
+import "./../../componentes/Detail/Detail.scss";
 
 function Detail() {
-
   const { id } = useParams();
   const { data: blog, isLoading, error } = useFetch(`${url}${id}`);
   const history = useHistory();
@@ -22,18 +22,24 @@ function Detail() {
   };
 
   return (
-    <div>
-      {isLoading && <div>Cargando...</div>}
-      {error && <div>{error}</div>}
+    <div className="detalle">
+      {isLoading && <div className="detalle_cargando">Cargando...</div>}
+      {error && <div className="detalle_error">{error}</div>}
       {blog && (
-        <article>
-          <h2>{blog.titulo}</h2>
-          <p>Escrito por: {blog.autor}</p>
-          <div>{blog.body}</div>
-          <button onClick={() => eliminarEntrada(blog.id)}>
-            ELiminar esta entrada
-          </button>
-        </article>
+        <div className="detalle__articulo">
+          <div className="detalle__titulo">{blog.titulo}</div>
+          <p className="detalle__autor">Escrito por: {blog.autor}</p>
+          <div className="detalle_body">
+            <p>{blog.body}</p>
+
+            <button
+              className="detalle__body__boton"
+              onClick={() => eliminarEntrada(blog.id)}
+            >
+              ELiminar esta entrada
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
