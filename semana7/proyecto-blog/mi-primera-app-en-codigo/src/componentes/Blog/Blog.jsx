d56398fd5d79e522697e6c39bca6b26f018d2aa1
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-23 20:39:21 -0500

    trabajando con sass

--- semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/Blog/Blog.jsx
+++ semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/Blog/Blog.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import "./Blog.css";
+import "./Blog.scss";
 function Blog(props) {
   const { titulo, body, autor, url, redirigir, id } = props;
   return (
     <div className="container">
-      <div className="subcontainer">
-        <div className="blog">
-          <p className="titulo">{titulo}</p>
-          <p className="cuerpo">{body}</p>
-          <p className="autor">Escrito por: {autor}</p>
-          <button onClick={() => redirigir(id)}>Ir a detalle</button>
+      <div className="container__subcontainer">
+        <div className="container__subcontainer__blog">
+          <p className="container__subcontainer__titulo">{titulo}</p>
+          <p className="container__subcontainer__cuerpo">{body}</p>
+          <p className="container__subcontainer__autor">Escrito por: {autor}</p>
+          <button
+            className="container__subcontainer__button"
+            onClick={() => redirigir(id)}
+          >
+            Ir a detalle
+          </button>
         </div>
-        <img className="imagen" src={url} alt="imagen" />
+        <img className="container__image" src={url} alt="imagen" />
       </div>
     </div>
   );
