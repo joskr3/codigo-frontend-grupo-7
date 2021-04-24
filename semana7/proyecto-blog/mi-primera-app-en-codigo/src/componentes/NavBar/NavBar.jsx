cd2690f2decc7759b8fc268f81f58c6cb9f4e0d6
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2021-04-23 21:50:39 -0500

    trabajando con react motion

--- semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/NavBar/NavBar.jsx
+++ semana7/proyecto-blog/mi-primera-app-en-codigo/src/componentes/NavBar/NavBar.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import logo from "../../assets/images/logotipo/logo-codigo.png";
 import { useHistory } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import "./NavBar.scss";
 
 function NavBar() {
   const history = useHistory();
   return (
-    <div className="header">
-      <img className="header__logo" src={logo} alt="logo" />
+    <motion.div
+      className="header"
+      initial={{ y: -150 }}
+      animate={{ y: 0 }}
+      transition={{ delay: 0.2, type: "spring", stiffness: 60 }}
+    >
+      <AnimatePresence>
+        <motion.img
+          className="header__logo"
+          src={logo}
+          alt="logo"
+          animate={{ x: 100 }}
+          transition={{ ease: "easeOut", duration: 2 }}
+        />
+      </AnimatePresence>
+
       <nav className="header__nav">
         <a className="header__nav__a" href="/">
           Home
@@ -39,7 +54,7 @@ function NavBar() {
           Iniciar sesion
         </a>
       </nav>
-    </div>
+    </motion.div>
   );
 }
 
